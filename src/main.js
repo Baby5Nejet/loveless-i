1f74b7d37341df4a8fab26c13b47b2964ee75cca
Baby777 <409178491@qq.com>
2021-02-14 23:23:39 +0800

    pro

--- src/main.js
+++ src/main.js
@@ -1,16 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 // import VueRouter from 'vue-router'
-import ViewUI from 'view-design';
-import 'view-design/dist/styles/iview.css';
-import router from './route';
+import ViewUI from 'view-design'
+import 'view-design/dist/styles/iview.css'
+import router from './route'
 // Vue.use(VueRouter)
-Vue.use(ViewUI);
-
+Vue.use(ViewUI)
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h  => h(App),
+  render: h => h(App),
   router
 }).$mount('#app')
--- src/view/home/Home.vue
+++ src/view/home/Home.vue
@@ -10,9 +10,84 @@
     <Divider dashed />
     <Modal
       v-model=" modal"
+      width="800"
       title="Common Modal dialog box title"
     >
-      <p>Content of dialog</p>
+      <Form
+        ref="fomData"
+        :model="fomData"
+      >
+        <Table
+          :loading="modalLoading"
+          :columns="modalCol"
+          :data="fomData.modalForm"
+        >
+          <template
+            slot="buzType"
+            slot-scope="{index}"
+          >
+            <FormItem>
+              <Select
+                v-model="fomData.modalForm[index].buzType"
+                :transfer="true"
+              >
+                <Option value="beijing">
+                  经营单元
+                </Option>
+                <Option value="shanghai">
+                  经营单元目标
+                </Option>
+              </Select>
+            </FormItem>
+          </template>
+          <template
+            slot="role"
+            slot-scope="{index}"
+          >
+            <FormItem>
+              <Select
+                v-model="fomData.modalForm[index].role"
+                multiple
+                :transfer="true"
+              >
+                <Option value="beijing">
+                  经营单元目标管理员
+                </Option>
+                <Option value="shanghai">
+                  经营单元目标超级管理员
+                </Option>
+              </Select>
+            </FormItem>
+          </template>
+          <template
+            slot="actType"
+            slot-scope="{index}"
+          >
+            {{ index }}
+          </template>
+          <template
+            slot="template"
+            slot-scope="{index}"
+          >
+            <FormItem>
+              <Select
+                v-model="fomData.modalForm[index].template"
+                :transfer="true"
+              >
+                <Option value="beijing">
+                  New York
+                </Option>
+                <Option value="shanghai">
+                  London
+                </Option>
+                <Option value="shenzhen">
+                  Sydney
+                </Option>
+              </Select>
+            </FormItem>
+          </template>
+        </Table>
+      </Form>
       <div slot="footer">
         <Button @click="addCancel()">
           Cancel
@@ -29,7 +104,54 @@
 export default {
   data () {
     return {
-      modal: false
+      modal: true,
+      modalLoading: false,
+      modalCol: [
+        {
+          title: '业务类型',
+          minWidth: 120,
+          slot: 'buzType',
+          renderHeader: (h, par) => {
+            return [h('span', { style: 'color:red' }, '*'),
+              h('span', ' ' + par.column.title)]
+          }
+        },
+        {
+          title: '需要提交审批的角色',
+          minWidth: 150,
+          slot: 'role',
+          renderHeader: (h, par) => {
+            return [h('span', { style: 'color:red' }, '*'),
+              h('span', ' ' + par.column.title)]
+          }
+        },
+        {
+          title: '操作类型',
+          minWidth: 120,
+          slot: 'actType',
+          renderHeader: (h, par) => {
+            return [h('span', { style: 'color:red' }, '*'),
+              h('span', ' ' + par.column.title)]
+          }
+        },
+        {
+          title: '审批链模板',
+          minWidth: 120,
+          slot: 'template',
+          renderHeader: (h, par) => {
+            return [h('span', { style: 'color:red' }, '*'),
+              h('span', ' ' + par.column.title)]
+          }
+        }
+      ],
+      fomData:
+      { modalForm: [
+        { buzType: '',
+          role: '',
+          actType: '',
+          template: '' }
+      ] }
+
     }
   },
   methods: {
